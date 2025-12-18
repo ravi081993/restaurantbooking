c92b1fd3db602f55cf4d9e209715f7c211d6e6c1
rohit <ryrohit.k@gmail.com>
2025-12-19 00:17:24 +0530

    chore: init repo for Java upgrade plan

---
+++ src/main/java/com/example/restaurantbooking/RestaurantbookingApplication.java
@@ -0,0 +1,13 @@
+package com.example.restaurantbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantbookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantbookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/restaurantbooking/RestaurantbookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restaurantbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantbookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
